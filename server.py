fd0bd26e502e25e317a22b65378ee9fc08ab1749
jibranabsarulislam <jga26@njit.edu>
2023-03-29 18:24:05 -0400

    edits

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # https://gist.github.com/HaiyangXu/ec88cbdce3cdbac7b8d5
-#Use to create local host
+# Use to create local host
 
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
 import socketserver
@@ -7,22 +7,25 @@ import socketserver
 PORT = 1337
 
 Handler = SimpleHTTPRequestHandler
-Handler.extensions_map.update({
-        '': 'application/octet-stream',
-        '.manifest': 'text/cache-manifest',
-        '.html': 'text/html',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.svg':	'image/svg+xml',
-        '.css':	'text/css',
-        '.js':'application/x-javascript',
-        '.wasm': 'application/wasm',
-        '.json': 'application/json',
-        '.xml': 'application/xml',
-});
+Handler.extensions_map.update(
+    {
+        "": "application/octet-stream",
+        ".manifest": "text/cache-manifest",
+        ".html": "text/html",
+        ".png": "image/png",
+        ".jpg": "image/jpg",
+        ".svg": "image/svg+xml",
+        ".css": "text/css",
+        ".js": "application/x-javascript",
+        ".wasm": "application/wasm",
+        ".json": "application/json",
+        ".xml": "application/xml",
+    }
+)
+
 
 def run(server_class=ThreadingHTTPServer, handler_class=Handler):
-    server_address = ('', PORT)
+    server_address = ("", PORT)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
